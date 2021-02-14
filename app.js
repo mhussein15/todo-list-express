de821931ec0ac4790494492515273594fa0de840
Mohammad Hussein <mhalfakih15@gmail.com>
2021-02-14 12:16:44 +0300

    todo-express-0.02

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const db = require("./db/models");
+
+const PORT = 8000;
+app.listen(PORT);
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
+db.sequelize.sync({ alter: true });
